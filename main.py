72a839ee9787a65c09606198f496957a21469ad3
Doctor <jebpip2008@gmail.com>
2023-02-15 20:54:10 +0300

    All variants calculation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import itertools
 from analytics import get_image_out_point
-
 
 if __name__ == '__main__':
     data = get_image_out_point("images/test.jpg")
+
+    all_variants = list(itertools.permutations(data.places))
     print()
