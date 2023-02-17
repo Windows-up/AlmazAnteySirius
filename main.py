bbe15ffc2f177de8c34440722c6c3e848c321f26
artem <artem.a.malyshev.2008@gmail.com>
2023-02-17 16:49:04 +0300

    Added

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import itertools
-from analytics import get_image_out_point
+from analytics import get_image_out_point, findDistance
+import math
+
+
+
 
 if __name__ == '__main__':
     data = get_image_out_point("images/test.jpg")
+    minElement = 9999999999
+    minPath = 0
+
+
+    def dist(p1, p2):
+        return math.hypot(p2[0] - p1[0], p2[1] - p1[1])
 
     all_variants = list(itertools.permutations(data.places))
-    print()
+    for i in range(len(all_variants)):
+        variant = list(all_variants[i])
+        variant.insert(0, data.garage)
+        variant.append(data.parking)
+        summ = 0
+        for j in range(len(all_variants[i])):
+            summ += dist(variant[j], variant[j+1])
+            if summ < minElement:
+                minPath = variant
+                minElement = summ
+
+    print(minElement, minPath)
+
+
+
+
+
+
