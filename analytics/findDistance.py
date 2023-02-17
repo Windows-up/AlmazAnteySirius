92b9621c714aef367d0bf00765a54840b447a862
artem <artem.a.malyshev.2008@gmail.com>
2023-02-17 19:01:31 +0300

    Added function that find min Path

--- analytics/findDistance.py
+++
@@ -1,2 +0,0 @@
-def distance(point_1, point_2):
-    return ((point_2[0] - point_1[0]) ** 2 + (point_2[1] - point_1[1]) ** 2) ** 0.5
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import itertools
-from analytics import get_image_out_point, findDistance
+from analytics import get_image_out_point
 import math
 
 
@@ -17,14 +17,14 @@ if __name__ == '__main__':
     all_variants = list(itertools.permutations(data.places))
     for i in range(len(all_variants)):
         variant = list(all_variants[i])
-        variant.insert(0, data.garage)
-        variant.append(data.parking)
+        variant.insert(0, data.garage[0])
+        variant.append(data.parking[0])
         summ = 0
-        for j in range(len(all_variants[i])):
+        for j in range(len(variant)-1):
             summ += dist(variant[j], variant[j+1])
-            if summ < minElement:
-                minPath = variant
-                minElement = summ
+        if summ < minElement:
+            minPath = variant
+            minElement = summ
 
     print(minElement, minPath)
 
